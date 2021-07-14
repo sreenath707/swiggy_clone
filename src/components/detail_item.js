f87e37ae230fc21e3214a183cea7fe086f3cb634
sreenath sharma <sreenathsharma707@gmail.com>
2021-07-14 13:40:57 +0530

    menu styled

--- src/components/detail_item.js
+++ src/components/detail_item.js
@@ -3,29 +3,57 @@ import {useState} from 'react';
 
 function Item(props){
     const [cost,setCost] = useState(0);
+    const [isAboveOne,SetAboveOne] = useState(false);
 
 
     function buttonIncrementHandler(){
-        setCost(prevCost=> ++prevCost);
+        setCost(prevCost=> {
+            if(prevCost == 0){
+                SetAboveOne(true);
+            }
+            return ++prevCost;
+        });
     }
     function buttonDecrementHandler(){
         if(cost >0){
-            setCost(prevCost=>--prevCost);
+            setCost(prevCost=>{
+                if(prevCost == 1){
+                    SetAboveOne(false);
+                }
+                return --prevCost;
+            });
         }
     }
+
+    function ButtonInside(props){
+        if(props.isAboveOne){
+            return(
+                <div className="item-button">
+                    <div onClick={buttonDecrementHandler}>
+                        <i className="bi bi-dash-lg" ></i>
+                    </div>
+                    <div>{cost}</div>
+                    <i className="bi bi-plus-lg" onClick={buttonIncrementHandler}></i>
+                </div>
+            )
+        }
+        else{
+            return(
+                <div className="item-button" onClick={buttonIncrementHandler}>ADD</div>
+            )
+        }
+    }
+
+    
     return(
-    <div key={props.item._id} className = " container props.item-card">
+    <div key={props.item._id} className = " container item-card">
         <div className="row">
             <div className="col-8">
                 <div className="item-name">{props.item._name}</div>
-                <div clasname="subtitle">{props.item._cost}</div>
+                <div clasname="subtitle">${props.item._cost}</div>
             </div>
-            <div className="item-button col-4" >
-                <div onClick={buttonDecrementHandler}>
-                    <i class="bi bi-dash-lg" ></i>
-                </div>
-                <div>{cost}</div>
-                <i className="bi bi-plus-lg" onClick={buttonIncrementHandler}></i>
+            <div className="col-4" >
+                <ButtonInside isAboveOne = {isAboveOne}></ButtonInside>
             </div>
         </div>                        
     </div>
--- src/components/details-body.js
+++ src/components/details-body.js
@@ -1,13 +1,13 @@
 import './detail_style.css';
 import Item from './detail_item';
-import {useState} from 'react-dom';
+import './dependencies/bootstrap/bootstrap.min.css'
 
 let dummyMenu =[
-     {_name : 'french fries', _id : 1, _cost : 150 },
-     {_name : 'french fries', _id : 2, _cost : 150 },
-     {_name : 'french fries', _id : 3, _cost : 150 },
-     {_name : 'french fries', _id : 4, _cost : 150 },
-     {_name : 'french fries', _id : 5, _cost : 150 }
+     {_name : 'French Fries', _id : 1, _cost : 150 },
+     {_name : 'French Fries', _id : 2, _cost : 150 },
+     {_name : 'French Fries', _id : 3, _cost : 150 },
+     {_name : 'French Fries', _id : 4, _cost : 150 },
+     {_name : 'French Fries', _id : 5, _cost : 150 }
 ];
 
 function DetailBody(){
@@ -16,13 +16,16 @@ function DetailBody(){
         <div className="container details-body">
             <div className="row">
                 <div className="col-7 details-menu">
+                    <h1 className="menu-menu">Menu</h1>
                     {dummyMenu.map((item)=>{
                         return(
                             <Item item={item}></Item>
                         )
                     })}
                 </div>
-                <div className="col-5 details-card"></div>
+                <div className="col-5 details-card">
+                    <h1 className="menu-menu">Cart</h1>
+                </div>
             </div>
         </div>
     )
